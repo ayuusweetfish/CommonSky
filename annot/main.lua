ec83c258ec6251b32b7fae9d0bed8f6e993c60ce
Shiqing <shiqing-thu18@yandex.com>
2022-09-30 10:03:14 +0800

    Separate reusable subroutines

--- annot/main.lua
+++ annot/main.lua
@@ -6,7 +6,10 @@ love.window.setMode(
   { fullscreen = false, highdpi = true }
 )
 
+require './utils'
+
 local imgpath = '../img'
+local annotpath = imgpath .. '/annot.txt'
 
 local path, name
 local img
@@ -20,78 +23,8 @@ local cx, cy, cr  -- Fit circle
 -- Saved annotations
 local annot = {}      -- name -> list of anchors
 local annotlist = {}  -- index -> name
-local saveannots      -- Defined later
 
 local selectedindex
-
-local fitcircle = function ()
-  if #anchors <= 2 then
-    cx, cy, cr = nil, nil, nil
-    return
-  end
-  -- Coope's method
-  local N = #anchors
-  local X = {}  -- N * 3
-  local Y = {}  -- N
-  for i = 1, N do
-    local ax, ay = unpack(anchors[i])
-    -- (ax - X)^2 + (ay - Y)^2 = R^2
-    local coeffx = -2 * ax
-    local coeffy = -2 * ay
-    local coeffc = -(ax * ax + ay * ay)
-    -- Cx * X + Cy * Y + 1 * D = Cc
-    -- where D = X^2 + Y^2 - R^2
-    X[#X + 1] = coeffx
-    X[#X + 1] = coeffy
-    X[#X + 1] = 1
-    Y[#Y + 1] = coeffc
-  end
-  -- Linear least squares
-  -- B = inv(X^T X) * X^T * Y
-  local XTX = {}  -- 3 * 3
-  for i = 1, 3 do
-    for j = 1, 3 do
-      local s = 0
-      for k = 1, N do
-        s = s + X[(k - 1) * 3 + i] * X[(k - 1) * 3 + j]
-      end
-      XTX[(i - 1) * 3 + j] = s
-    end
-  end
-  local XTY = {}  -- 3
-  for i = 1, 3 do
-    local s = 0
-    for k = 1, N do
-      s = s + X[(k - 1) * 3 + i] * Y[k]
-    end
-    XTY[i] = s
-  end
-  -- Invert (X^T X)
-  local a11, a12, a13, a21, a22, a23, a31, a32, a33 = unpack(XTX)
-  local d =   -- Determinant
-      a11 * (a22 * a33 - a32 * a23)
-    - a21 * (a12 * a33 - a13 * a32)
-    + a31 * (a12 * a23 - a13 * a22)
-  local invXTX = {
-     (a22*a33 - a32*a23)/d, -(a12*a33 - a13*a32)/d,  (a12*a23 - a13*a22)/d,
-    -(a21*a33 - a23*a31)/d,  (a11*a33 - a13*a31)/d, -(a11*a23 - a13*a21)/d,
-     (a21*a32 - a22*a31)/d, -(a11*a32 - a12*a31)/d,  (a11*a22 - a12*a21)/d,
-  }
-  -- Solution to LLS
-  local B = {}
-  for i = 1, 3 do
-    local s = 0
-    for k = 1, 3 do
-      s = s + invXTX[(i - 1) * 3 + k] * XTY[k]
-    end
-    B[i] = s
-  end
-  -- for i = 1, 3 do print(B[i]) end
-  cx = B[1]
-  cy = B[2]
-  cr = math.sqrt(cx * cx + cy * cy - B[3])
-  -- print(cx, cy, cr)
-end
 
 local updateimage = function ()
   name = annotlist[selectedindex]
@@ -104,7 +37,7 @@ local updateimage = function ()
   ioffy = (H - ih * isc) / 2
   anchors = annot[name]
   anchorsel = nil
-  fitcircle()
+  cx, cy, cr = fitcircle(anchors)
 end
 
 -- Screen coordinates to image coordinates
@@ -133,7 +66,7 @@ function love.mousepressed(x, y, button, istouch, presses)
       else
         -- Remove
         table.remove(anchors, i)
-        fitcircle()
+        cx, cy, cr = fitcircle(anchors)
         return
       end
       break
@@ -144,7 +77,7 @@ function love.mousepressed(x, y, button, istouch, presses)
     anchors[#anchors + 1] = {ix, iy}
     anchorsel = #anchors
     anchorseloffx, anchorseloffy = 0, 0
-    fitcircle()
+    cx, cy, cr = fitcircle(anchors)
   end
 end
 
@@ -153,13 +86,13 @@ function love.mousemoved(x, y, button, istouch, presses)
     local ix, iy = icoord(x + anchorseloffx, y + anchorseloffy)
     anchors[anchorsel][1] = ix
     anchors[anchorsel][2] = iy
-    fitcircle()
+    cx, cy, cr = fitcircle(anchors)
   end
 end
 
 function love.mousereleased(x, y, button, istouch, presses)
   anchorsel = nil
-  saveannots()
+  saveannots(annotpath, annot, annotlist)
 end
 
 love.keyboard.setKeyRepeat(true)
@@ -173,56 +106,14 @@ function love.keypressed(key, scancode, isrepeat)
   end
 end
 
--- Load saved annotations
--- On new images, run:
--- find . -name "*.jpg" -exec basename {} \; >> annot.txt
-local f = io.open(imgpath .. '/annot.txt', 'r')
-while true do
-  local s = f:read('*l')
-  if s == nil then break end
-  local index = s:find('\t')
-  if index ~= nil then
-    local name = s:sub(1, index - 1)
-    local anchors = {}
-    while index ~= nil do
-      local nextindex1 = s:find('\t', index + 1)
-      local nextindex2 = s:find('\t', nextindex1 + 1)
-      local x = tonumber(s:sub(index + 1, nextindex1 - 1))
-      local y = tonumber(s:sub(nextindex1 + 1, (nextindex2 or 0) - 1))
-      anchors[#anchors + 1] = {x, y}
-      index = nextindex2
-    end
-    annot[name] = anchors
-    annotlist[#annotlist + 1] = name
-  elseif annot[s] == nil then
-    annot[s] = {}
-    annotlist[#annotlist + 1] = s
-  end
-end
-f:close()
-
-saveannots = function ()
-  local f = io.open(imgpath .. '/annot.txt', 'w')
-  for i = 1, #annotlist do
-    local name = annotlist[i]
-    f:write(name)
-    local a = annot[name]
-    for j = 1, #a do
-      f:write('\t', a[j][1], '\t', a[j][2])
-    end
-    f:write('\n')
-  end
-  f:close()
-end
-saveannots()
+annot, annotlist = loadannots(annotpath)
 
 if os.getenv('stats') ~= nil then
   local hist = {}
   for i = 0, 180 do hist[i] = 0 end
   for i = 1, #annotlist do
     local a = annot[annotlist[i]]
-    anchors = a
-    fitcircle()
+    local cx, cy, cr = fitcircle(a)
     if cx ~= nil then
       local min, max = 180, 0
       for j = 1, #a do
