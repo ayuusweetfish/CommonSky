3dcdd1ce72da8323c04f1ac257714a30fc263e91
Shiqing <shiqing-thu18@yandex.com>
2022-10-03 20:51:53 +0800

    ✨ Add matrix inv-mul subroutine

--- astra/align/polyfit.c
+++ astra/align/polyfit.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// C (n*m) = A^-1 (n*n) B (n*m)
+static void invert_mul(int n, int m, double *a, double *b, double *c);
+
 // For 0<=k<n, 0<=c<=1:
 // v[2k+c] =
 //  let ux = u[2k+0], uy = u[2k+1]
@@ -27,41 +30,15 @@ void polyfit(int n, double *u, double *v, int ord, double *o_coeff)
         XTX[j][k] +=
           (j == 2 ? 1 : u[i * 2 + j]) *
           (k == 2 ? 1 : u[i * 2 + k]);
-  // Invert
-  #define a11 XTX[0][0]
-  #define a12 XTX[0][1]
-  #define a13 XTX[0][2]
-  #define a21 XTX[1][0]
-  #define a22 XTX[1][1]
-  #define a23 XTX[1][2]
-  #define a31 XTX[2][0]
-  #define a32 XTX[2][1]
-  #define a33 XTX[2][2]
-  double d =
-      a11 * (a22 * a33 - a32 * a23)
-    - a21 * (a12 * a33 - a13 * a32)
-    + a31 * (a12 * a23 - a13 * a22);
-  if (fabs(d) <= 1e-12) {
-    printf("Non-invertible matrix (X^T X)!");
-    exit(1);
-  }
-  double invXTX[3][3] = {
-    { (a22*a33 - a32*a23)/d, -(a12*a33 - a13*a32)/d,  (a12*a23 - a13*a22)/d},
-    {-(a21*a33 - a23*a31)/d,  (a11*a33 - a13*a31)/d, -(a11*a23 - a13*a21)/d},
-    { (a21*a32 - a22*a31)/d, -(a11*a32 - a12*a31)/d,  (a11*a22 - a12*a21)/d},
-  };
   // X^T * Y
   double XTY[3][2] = {{ 0 }};
   for (int i = 0; i < n; i++)
     for (int j = 0; j < 3; j++)
       for (int k = 0; k < 2; k++)
         XTY[j][k] += (j == 2 ? 1 : u[i * 2 + j]) * v[i * 2 + k];
-  // Multiply
-  double LLS_sol[3][2] = {{ 0 }};
-  for (int i = 0; i < 3; i++)
-    for (int j = 0; j < 2; j++)
-      for (int k = 0; k < 3; k++)
-        LLS_sol[i][j] += invXTX[i][k] * XTY[k][j];
+  // Invert and multiply
+  double LLS_sol[3][2];
+  invert_mul(3, 2, &XTX[0][0], &XTY[0][0], &LLS_sol[0][0]);
   // Copy to coefficients as a starting point
   for (int c = 0; c < 2; c++) {
     C(c, 0, 0) = LLS_sol[2][c];
@@ -91,4 +68,53 @@ void polyapply(int n, double *u, double *o_v, int ord, double *coeff)
       o_v[k * 2 + c] = sum;
     }
   }
+}
+
+static void invert_mul(int n, int m, double *a, double *b, double *c)
+{
+  double *aux = (double *)malloc(sizeof(double) * n * (n + m));
+  #define A(_i, _j) aux[(_i) * (n + m) + (_j)]
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++)
+      A(i, j) = a[i * n + j];
+    for (int j = 0; j < m; j++)
+      A(i, j + n) = b[i * m + j];
+  }
+
+  for (int i = 0; i < n; i++) {
+    int pivot = i;
+    double maxabs = fabs(A(i, i));
+    for (int j = i + 1; j < n; j++)
+      if (maxabs < fabs(A(j, i))) {
+        maxabs = fabs(A(j, i));
+        pivot = j;
+      }
+    if (pivot != i)
+      for (int j = i; j < n + m; j++) {
+        double t = A(i, j);
+        A(i, j) = A(pivot, j);
+        A(pivot, j) = t;
+      }
+    for (int j = i + 1; j < n; j++) {
+      double factor = A(j, i) / A(i, i);
+      for (int k = i; k < n + m; k++)
+        A(j, k) -= factor * A(i, k);
+    }
+  }
+  for (int i = n - 1; i >= 0; i--) {
+    for (int j = i + 1; j < n + m; j++)
+      A(i, j) /= A(i, i);
+    A(i, i) = 1;
+    for (int j = 0; j < i; j++)
+      for (int k = n + m - 1; k >= i; k--)
+        A(j, k) -= A(j, i) * A(i, k);
+  }
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      c[i * m + j] = A(i, j + n);
+
+  #undef A
+  free(aux);
 }
