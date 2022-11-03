20598a25dee14e097d745163376cf2dec60acc34
Shiqing <shiqing-thu18@yandex.com>
2022-11-03 20:24:49 +0800

    🌈 Almost-complete list of sources

--- astra/sources/wherefroms.sh
+++ astra/sources/wherefroms.sh
@@ -2,9 +2,11 @@
 # Based on https://github.com/jakepetroules/wherefrom
 set -o pipefail
 for file in ../img/*; do
-  xattr -p com.apple.metadata:kMDItemWhereFroms "$file" \
-    | xxd -r -p \
-    | plutil -convert xml1 -o - - \
-    | grep "string" \
-    | cat <(echo $(basename $file)) -
+  contents=$(xattr -p com.apple.metadata:kMDItemWhereFroms "$file")
+  if [ "$?" == 0 ]; then
+    xxd -r -p <<< "$contents" \
+      | plutil -convert xml1 -o - - \
+      | grep "string" \
+      | cat <(echo $(basename $file)) -
+  fi
 done
